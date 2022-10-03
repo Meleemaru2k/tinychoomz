bde230a334a5d8be3e9372f50f87a0785aafc820
Emerald Hedgehog <s.boergers@live.de>
2022-10-03 03:54:29 +0200

    setup

--- src/game/src/job/job.ts
+++ src/game/src/job/job.ts
@@ -1,18 +1,21 @@
 export class Job {
 	id: string;
 	name: string;
+	description: string;
 	sequences: array<JobSequence>;
+	activeSequence: number;
 	//constructor() {}
 }
 
 export class JobSequence {}
 
-export class JobSequenceStep {}
-
-export class JobSequenceDialog {
-	dialogues: Array<{ text: string; image: string; options: Array<string> }>;
+export interface JobDialog {
+	dialogues: Array<{
+		text: string;
+		image: string;
+		choices: Array<{ id: number; name: string; text: string; limitedTo: object }>;
+	}>;
 }
-
-export class JobSequenceEncounter {
+export interface JobEncounter {
 	encounter: { placeholder: Array<object> };
 }
